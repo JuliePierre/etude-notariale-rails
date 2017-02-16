94ee496749d8b3f5553057857bba6309de6f91eb
Julie Pierre <julie.pierre3@gmail.com>
2017-02-16 20:41:44 +0100

    changement de front de la page d'accueil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   require 'rss'
   require 'open-uri'
 
-  skip_before_action :authenticate_user!, only: [ :home, :office, :expertises, :droit_famille, :mariage, :pacs, :donation, :testament, :droit_immobilier, :affaires, :promotion, :guides, :contact ]
+  skip_before_action :authenticate_user!, only: [ :home, :office, :expertises, :famille, :couple, :mariage, :pacs, :divorce, :succession, :transmission, :donation, :testament, :adoption, :affaires, :societe, :acquisition_cession, :bail_commercial, :droit_immobilier, :acheter, :vendred, :promotion, :faq, :contact, :negociation ]
 
   def home
     @results = Expertise.search('test')
@@ -11,20 +11,47 @@ class PagesController < ApplicationController
   def office
   end
 
+  # Partie expertises
   def expertises
+    # 4 grandes expertises :
+    # 1. droit de la famille
+    # 2. droit des affaires
+    # 3. droit immobilier
+    # 4. promotion immobilière
   end
 
-  def droit_famille
+  # Droit de la famille
+  def famille
+    # 1. Couple
+    # 2. Succession
+    # 3. Transmission (Donation - Testament)
+    # 4. Adoption
+  end
+
+  # 1. Couple
+  def couple
+    # 1. Mariage
+    # 2. Pacs
+    # 3. Divorce
   end
 
   def mariage
-    @expertise = Expertise.find_by(nom: 'Mariage')
-    @introduction = @expertise.introduction
-    @description = @expertise.description
-    @questions = @expertise.questions
   end
 
   def pacs
+  end
+
+  def divorce
+  end
+
+  # 2. Succession
+  def succession
+  end
+
+  # 3. Transmission
+  def transmission
+    # 1. Donation
+    # 2. Testament
   end
 
   def donation
@@ -33,16 +60,39 @@ class PagesController < ApplicationController
   def testament
   end
 
+  # Droit des affaires
+  def affaires
+    # 1. Société
+    # 2. Acquisition / Cession
+    # 3. Bail commercial
+    # 4. Secrétariat juridique de l'entreprise
+  end
+
+  def societe
+  end
+
+  def acquisition_cession
+  end
+
+  def bail_commercial
+  end
+
+  # Droit immobilier
   def immobilier
   end
 
-  def affaires
+  def acheter
   end
+
+  def vendre
+  end
+
 
   def promotion
   end
+  # Fin des expertises
 
-  def guides
+  def faq
     url = 'https://www.notaires.fr/fr/actualites-rss'
     open(url) do |rss|
       @feed_actualites = RSS::Parser.parse(rss)
@@ -58,6 +108,7 @@ class PagesController < ApplicationController
     @contact_types = Contact::CONTACTTYPES
   end
 
+  # Négociation immobilière
   def negociation
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,41 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'office', to: 'pages#office', as: 'office'
-  get 'expertises', to: 'pages#expertises', as: 'expertises'
-  get 'expertises/droit-de-la-famille', to: 'pages#droit_famille', as: 'famille'
-  get 'expertises/droit-de-la-famille/mariage', to: 'pages#mariage', as: 'mariage'
-  get 'expertises/droit-de-la-famille/pacs', to: 'pages#pacs', as: 'pacs'
-  get 'expertises/droit-de-la-famille/donation', to: 'pages#donation', as: 'donation'
-  get 'expertises/droit-de-la-famille/testament', to: 'pages#testament', as: 'testament'
-  get 'expertises/droit-immobilier', to: 'pages#immobilier', as: 'immobilier'
-  get 'expertises/droit-des-affaires', to: 'pages#affaires', as: 'affaires'
-  get 'expertises/promotion-immobiliere', to: 'pages#promotion', as: 'promotion'
-  get 'guides-pratiques', to: 'pages#guides', as: 'guides'
+  get 'foire-aux-questions', to: 'pages#faq', as: 'faq'
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'negociation-immobiliere', to: 'pages#negociation', as: 'negociation'
   get 'vente-immobilier', to: 'pages#vente', as: 'vente'
+
+  # Routes des pages expertises dans l'ordre
+  get 'expertises', to: 'pages#expertises', as: 'expertises'
+  get 'expertises/search', to: 'expertises#search', as: 'search'
+
+  # Droit de la famille
+  get 'expertises/droit-de-la-famille', to: 'pages#famille', as: 'famille'
+  get 'expertises/droit-de-la-famille/couple', to: 'pages#couple', as: 'couple'
+  get 'expertises/droit-de-la-famille/mariage', to: 'pages#mariage', as: 'mariage'
+  get 'expertises/droit-de-la-famille/pacs', to: 'pages#pacs', as: 'pacs'
+  get 'expertises/droit-de-la-famille/divorce', to: 'pages#divorce', as: 'divorce'
+  get 'expertises/droit-de-la-famille/succession', to: 'pages#succession', as: 'succession'
+  get 'expertises/droit-de-la-famille/transmission', to: 'pages#transmission', as: 'transmission'
+  get 'expertises/droit-de-la-famille/donation', to: 'pages#donation', as: 'donation'
+  get 'expertises/droit-de-la-famille/testament', to: 'pages#testament', as: 'testament'
+  get 'expertises/droit-de-la-famille/adoption', to: 'pages#adoption', as: 'adoption'
+
+  # Droit des affaires
+  get 'expertises/droit-des-affaires', to: 'pages#affaires', as: 'affaires'
+  get 'expertises/droit-des-affaires/societe', to: 'pages#societe', as: 'societe'
+  get 'expertises/droit-des-affaires/acquisition-cession', to: 'pages#acquisition_cession', as: 'acquisition_cession'
+  get 'expertises/droit-des-affaires/bail-commercial', to: 'pages#bail_commercial', as: 'bail_commercial'
+
+  # Droit immobilier
+  get 'expertises/droit-immobilier', to: 'pages#immobilier', as: 'immobilier'
+  get 'expertises/droit-immobilier/acheter', to: 'pages#acheter', as: 'acheter'
+  get 'expertises/droit-immobilier/vendre', to: 'pages#vendre', as: 'vendre'
+
+  # Promotion immobilière
+  get 'expertises/promotion-immobiliere', to: 'pages#promotion', as: 'promotion'
+
 
   resources :annonces, only: [:index, :show, :create, :update, :destroy]
   get 'annonces-immobilieres/admin', to: 'annonces#admin_index', as: 'tests'
@@ -23,5 +45,4 @@ Rails.application.routes.draw do
 
   resources :contacts, only: [:new, :create]
 
-  get 'expertises/search', to: 'expertises#search', as: 'search'
 end
