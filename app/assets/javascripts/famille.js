0aa3871bc9ba9818c6f994d4abef31eab08769a6
Julie Pierre <julie.pierre3@gmail.com>
2017-02-20 19:01:20 +0100

    WIP

--- app/assets/javascripts/famille.js
+++ app/assets/javascripts/famille.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  var top = $("#famille-menu-scrollspy").position()['top'];
+  // var familleMenuTop = $("#famille-menu-scrollspy").position()['top'];
+  var mariageMenuTop = $("#mariage-menu-scrollspy").position()['top'];
   $(window).scroll(function(){
-    $("#famille-menu-scrollspy").css("top", Math.max(80, top - $(this).scrollTop()));
+    console.log(mariageMenuTop);
+    // $("#famille-menu-scrollspy").css("top", Math.max(80, familleMenuTop - $(this).scrollTop()));
+    $("#mariage-menu-scrollspy").css("top", Math.max(80, mariageMenuTop - $(this).scrollTop()));
   });
+
 })
