5952d2f366f66ad030bbf75e67e81dda6e38b577
Julie Pierre <julie.pierre3@gmail.com>
2017-02-23 14:50:48 +0100

    logique recherche et présentation des résultats ok

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,4 @@
+// SEARCH PAGE //
 ;$('#navbar-search-icon').click(function(){
   $('.search-page-wrapper').addClass('active');
   $('.search-page-item').removeClass('hidden');
@@ -7,3 +8,18 @@ $('#search-page-close-link').click(function(){
   $('.search-page-wrapper').removeClass('active');
   $('.search-page-item').addClass('hidden');
 });
+// SEARCH PAGE //
+
+// SEARCH RESULTS //
+;$(document).ready(function(){
+  var maxL = 250;
+  $('.search-answer > p').each(function(i, item) {
+    var text = $(item).text();
+    console.log(text);
+    if(text.length > maxL) {
+      var begin = text.substr(0, maxL);
+      $(item).html(begin).append('...');
+    };
+  });
+});
+// SEARCH RESULTS //
--- app/controllers/expertises_controller.rb
+++ app/controllers/expertises_controller.rb
@@ -1,8 +1,8 @@
 class ExpertisesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:search]
   def search
-    search_test = params[:search]
-    @questions = Question.search(search_test)
-    @expertises = Expertise.search(search_test)
+    @search = params[:search]
+    @questions = Question.search(@search)
+    @expertises = Expertise.search(@search)
   end
 end
