24446b7c4a69b1c79dccdd3876c1f8626afc77b0
Julie Pierre <julie.pierre3@gmail.com>
2017-02-27 14:04:44 +0100

    modification de la config production pour integrer le mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'etude-notariale.herokuapp.com' }
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "etude-notariale-rails_#{Rails.env}"
