e7bbfa88ca2237bd7eb820555f49e88055c0d966
Julie Pierre <julie.pierre3@gmail.com>
2017-04-25 11:19:14 +0200

    meta tags en cours de modification

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,13 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : 'Office Notarial Perraut - Pirioux - Mével'
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : 'Office Notarial Perraut - Pirioux - Mével'
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
-    content_for?(:meta_image) ? content_for(:meta_image) : 'https://unsplash.com/?photo=P3pI6xzovu0'
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
 end
