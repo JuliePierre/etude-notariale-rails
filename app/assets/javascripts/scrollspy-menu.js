aadfced082dc48dbe042b1b634c19b6a2f0d3460
Julie Pierre <julie.pierre3@gmail.com>
2017-10-31 15:32:43 +0100

    solving issue with active admin js

--- app/assets/javascripts/scrollspy-menu.js
+++
@@ -1,6 +0,0 @@
-$('.menu-link').click(function(e) {
-  e.preventDefault();
-  var targetElement = $(this).data('link');
-  var targetPosition = $(targetElement).offset().top - 70;
-  $('html, body').animate({ scrollTop: targetPosition }, 'slow');
-});
